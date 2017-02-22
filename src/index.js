fbc0a6b3c0c05c0b615172b985b09d8d7197882c
ruanyl <ruanyu1@gmail.com>
2017-02-22 17:41:23 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export default (arr, str, customConfig) => {
+  const config = { letterFromStart: true, wordFromStart: false, ...customConfig }
+
+  const allStr = arr.join('').toUpperCase()
+  let wordIndexes = []
+  let firstLetters = ''
+  let isMatchLetter = false
+  let isMatchWord = false
+
+  arr.forEach((word, i) => {
+    wordIndexes = wordIndexes.concat(i === 0 ? 0 : arr[i - 1].length)
+    firstLetters = `${firstLetters}${word.charAt(0).toUpperCase()}`
+  })
+
+  if (config.letterFromStart) {
+    isMatchLetter = firstLetters.indexOf(str.toUpperCase()) === 0
+  } else {
+    isMatchLetter = firstLetters.indexOf(str.toUpperCase()) >= 0
+  }
+
+  if (config.wordFromStart) {
+    isMatchWord = allStr.indexOf(str.toUpperCase()) === 0
+  } else {
+    isMatchWord = wordIndexes.includes(allStr.indexOf(str.toUpperCase()))
+  }
+
+  return isMatchLetter || isMatchWord
+}
